ae7ebf7bb9c08e2dafa7e3a8191d330afcf15c9b
JunFe23 <ogum0405@hanmail.net>
2021-07-13 20:13:05 +0900

    subscribe 수정

--- src/main/java/com/cos/photogramstart/service/SubscribeService.java
+++ src/main/java/com/cos/photogramstart/service/SubscribeService.java
@@ -28,7 +28,7 @@ public class SubscribeService {
 		// 쿼리 준비
 		StringBuffer sb = new StringBuffer();
 		sb.append("SELECT u.id, u.username, u.profileImageUrl, ");
-		sb.append("if((SELECT 1 FROM subscribe WHERE fromUserId = ? AND toUserId = u.id), 1, 0) subscribeState, ");
+		sb.append("if((SELECT 1 FROM Subscribe WHERE fromUserId = ? AND toUserId = u.id), 1, 0) subscribeState, ");
 		sb.append("if((?=u.id), 1, 0) equalUserState ");
 		sb.append("FROM User u INNER JOIN subscribe s ");
 		sb.append("ON u.id = s.toUserId ");
